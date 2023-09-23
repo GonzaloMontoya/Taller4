eda5a02c602865f6a98661a52b46366967163646
GonzaloMontoya <go.montoya@duocuc.cl>
2023-09-23 14:23:09 -0300

    Rick&Morty_API

--- Taller4/src/app/home/home.page.ts
+++ Taller4/src/app/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RickmortyService } from './service/rickmorty.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
 
+export class HomePage implements OnInit {
+  personajes:any;
+  
+  constructor
+  ( private rickmortyservice: RickmortyService)
+  { }
+
+
+  ngOnInit(){
+    this.rickmortyservice.getCharacters().subscribe((_personajes:any)=>{
+      console.log(_personajes)
+      this.personajes = _personajes.results;
+    })
+  }
 }
